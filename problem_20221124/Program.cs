2a875d399214db751970873a023a192ceb892e95
Igor Xavier <igor.xavier.simoes@gmail.com>
2022-11-26 09:35:23 -0300

    desafios

---
+++ problem_20221124/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = { 10, 15, 3, 7, 4, 5, 26 };
+int k = 30;
+
+for (int i = 0; i < arr.Length;)
+{
+    for (int j = 0; j < arr.Length;)
+    {
+        int soma = Array.IndexOf(arr, arr[i]) + arr[j];
+
+        if (soma == k && arr[i] != arr[j])
+        {
+            Console.WriteLine("Posicao 1: " + arr[i] + " " + "Posicao 2: " + Array.IndexOf(arr, arr[j]));
+            return;
+        }
+        j++;
+    }
+    i++;
+}
---
+++ problem_20221125/Program.cs
@@ -0,0 +1,27 @@
+﻿int [] arr = { 1, 2, 3, 4, 5 };
+int[] numeros = new int [arr.Length] ;
+int mult = 1;
+for (int i = 0;i < arr.Length; i++)
+{
+    mult = mult * arr[i];
+}
+
+for (int i = 0; i < arr.Length;)
+{
+    int soma = arr[i];
+    int count = 1;
+    while(soma < mult)
+    {
+        soma += arr[i];
+        count++;
+    }
+    numeros[i] = count;
+    i++;
+}
+
+foreach (int i in numeros)
+{
+    Console.WriteLine(i);
+}
+
+
